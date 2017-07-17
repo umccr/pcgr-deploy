4e467949fc509f0e3f4cde0fe36083408fbefe65
Roman Valls Guimera <brainstorm@nopcode.org>
2017-07-17 15:52:34 +0200

    Switching to python3 due to newer ubuntu releases. Still timeouts after terraform local-exec and several issues with mount points and remounting :/

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,10 +8,10 @@ resource "aws_instance" "pcgr" {
   security_groups = ["default"]
   key_name = "mba_wwcrc"
   ebs_block_device {
-      device_name = "/dev/sdf"
+      device_name = "/dev/xvdf"
       volume_size = "${var.volsize}"
   } 
   provisioner "local-exec" {
-    command = "ansible-playbook ../ansible/site.yml -i ${aws_instance.pcgr.public_dns},"
+    command = "ansible-playbook ../ansible/site.yml -T 60 -e 'ansible_python_interpreter=/usr/bin/python3' -i ${aws_instance.pcgr.public_dns},"
   }
 }
