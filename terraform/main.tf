d01080c6bd2ef4268f02d6769d1163292cd72c5d
Roman Valls Guimera <brainstorm@nopcode.org>
2017-07-18 15:06:39 +0200

    Hardcode availability zone to same one from the instance EBS volume? Have sequential device name? No error logs on terraform anywhere yet EBS volumes keep not showing up :/

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,18 +9,19 @@ resource "aws_instance" "pcgr" {
   key_name = "mba_wwcrc"
   
   provisioner "local-exec" {
-    command = "ansible-playbook ../ansible/site.yml -T 60 -e 'ansible_python_interpreter=/usr/bin/python3' -i ${aws_instance.pcgr.public_dns},"
+    command = "sleep 30 && ansible-playbook ../ansible/site.yml -T 60 -e 'ansible_python_interpreter=/usr/bin/python3' -i ${aws_instance.pcgr.public_dns},"
   }
 }
 
 resource "aws_ebs_volume" "pcgr_volume"  {
-      availability_zone = "${aws_instance.pcgr.availability_zone}"
+      #availability_zone = "${aws_instance.pcgr.availability_zone}"
+      availability_zone = "ap-southeast-2a"
       type = "gp2"
       size = "${var.volsize}"
 } 
 
 resource "aws_volume_attachment" "pcgr_volume_attach" {
-  device_name = "/dev/xvdf"
+  device_name = "/dev/xvdb"
   instance_id = "${aws_instance.pcgr.id}"
   volume_id   = "${aws_ebs_volume.pcgr_volume.id}"
 }
