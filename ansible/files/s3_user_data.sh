a053e30dc825cf2e5f518f2d21740613c9530c61
Roman Valls Guimera <brainstorm@nopcode.org>
2018-01-09 15:47:07 +1100

    First working sample submission userdata w/ ansible

--- ansible/files/s3_user_data.sh
+++ ansible/files/s3_user_data.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-BUCKET="s3://umccr-pcgr/"
+export BUCKET="s3://umccr-pcgr/"
 
 cd /mnt/pcgr
-latest_vcf=`aws s3 ls $BUCKET --recursive *.vcf.gz | sort | tail -n 1 | awk '{print $4}’`
+sudo chown -R ubuntu:ubuntu /mnt/pcgr-0.5.3
+latest_vcf=`aws s3 ls $BUCKET | sort | tail -n 1 | grep -v output |  awk '{print $4}'`
+aws s3 cp ${BUCKET}${latest_vcf} .
+tabix $latest_vcf
 python pcgr.py --input_vcf $latest_vcf . output pcgr_configuration.toml $latest_vcf
-aws s3 cp —recursive output ${BUCKET}/${latest_vcf}-output
+aws s3 cp --recursive output ${BUCKET}${latest_vcf}-output
