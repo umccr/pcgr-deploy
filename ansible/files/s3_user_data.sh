351d92961f7463530a99cb6fd4b38eb179860773
Roman Valls Guimera <brainstorm@nopcode.org>
2018-01-10 16:27:40 +1100

    Add hotfixed validation, referring to umccr docker container. Thanks @vladsaveliev

--- ansible/files/s3_user_data.sh
+++ ansible/files/s3_user_data.sh
@@ -3,11 +3,12 @@
 export BUCKET="s3://umccr-pcgr/"
 
 sudo apt install -y awscli
+sudo cp /usr/share/zoneinfo/Australia/Melbourne /etc/localtime
 sudo mount /dev/xvdb /mnt/work
 cd /mnt/work/pcgr
 sudo chown -R ubuntu:ubuntu /mnt/work/pcgr-0.5.3
 latest_vcf=`aws s3 ls $BUCKET | sort | tail -n 1 | grep -v output |  awk '{print $4}'`
 aws s3 cp ${BUCKET}${latest_vcf} .
 tabix $latest_vcf
-python pcgr.py --no-validate --input_vcf $latest_vcf . output pcgr_configuration.toml $latest_vcf
+python pcgr.py --no_validate --input_vcf $latest_vcf . output pcgr_configuration.toml $latest_vcf
 aws s3 cp --recursive output ${BUCKET}${latest_vcf}-output
