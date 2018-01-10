8e24e65b9db2094dad3ef191a922930482e8d042
Roman Valls Guimera <brainstorm@nopcode.org>
2018-01-10 15:15:51 +1100

    Broken VCF validation on @sigven PCGR 0.5.x, so ignore validation for now /cc @vladsaveliev

--- ansible/files/s3_user_data.sh
+++ ansible/files/s3_user_data.sh
@@ -9,5 +9,5 @@ sudo chown -R ubuntu:ubuntu /mnt/work/pcgr-0.5.3
 latest_vcf=`aws s3 ls $BUCKET | sort | tail -n 1 | grep -v output |  awk '{print $4}'`
 aws s3 cp ${BUCKET}${latest_vcf} .
 tabix $latest_vcf
-python pcgr.py --input_vcf $latest_vcf . output pcgr_configuration.toml $latest_vcf
+python pcgr.py --no-validate --input_vcf $latest_vcf . output pcgr_configuration.toml $latest_vcf
 aws s3 cp --recursive output ${BUCKET}${latest_vcf}-output
