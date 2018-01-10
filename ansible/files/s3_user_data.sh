a97d5686a8368360158e692ab2a40fd14089b2ba
Roman Valls Guimera <brainstorm@nopcode.org>
2018-01-10 16:35:59 +1100

    Final tweaks on the PCGR hotfix

--- ansible/files/s3_user_data.sh
+++ ansible/files/s3_user_data.sh
@@ -9,6 +9,7 @@ cd /mnt/work/pcgr
 sudo chown -R ubuntu:ubuntu /mnt/work/pcgr-0.5.3
 latest_vcf=`aws s3 ls $BUCKET | sort | tail -n 1 | grep -v output |  awk '{print $4}'`
 aws s3 cp ${BUCKET}${latest_vcf} .
-tabix $latest_vcf
-python pcgr.py --no_validate --input_vcf $latest_vcf . output pcgr_configuration.toml $latest_vcf
+tabix -f $latest_vcf
+wget https://raw.githubusercontent.com/brainstorm/pcgr-deploy/master/ansible/files/pcgr_defaults.toml
+python pcgr.py --no_validate --input_vcf $latest_vcf . output pcgr_defaults.toml $latest_vcf
 aws s3 cp --recursive output ${BUCKET}${latest_vcf}-output
