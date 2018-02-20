586b46ce04627181ae59a0a352f48241b6f530ec
Roman Valls Guimera <brainstorm@nopcode.org>
2018-02-21 10:14:54 +1100

    Update on awscli parameters for off-arteria launch (debugging). Trying 'new' m5 instances

--- aws/cli/awscli_launch.sh
+++ aws/cli/awscli_launch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-aws ec2 request-spot-instances --spot-price "0.06" --instance-count 1 --type "one-time" --launch-specification file://launch_spec.json
+aws ec2 request-spot-instances --spot-price "0.08" --instance-count 1 --type "one-time" --launch-specification file://launch_spec.json
